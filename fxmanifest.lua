ba27e77c63042d753ff3ab3a970c14429ee60f31
draobrehtom <draobrehtom@gmail.com>
2023-08-30 16:14:39 +0200

    Init

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'draobrehtom'
+
+this_is_a_map 'yes'
+
+
+files {
+    "stream/arena_test.ytyp",
+}
+data_file "DLC_ITYP_REQUEST" "stream/arena_test.ytyp"
