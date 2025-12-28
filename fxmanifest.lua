c2bfd1566a23035763fd9b128216ace2e4d985dc
draobrehtom <draobrehtom@gmail.com>
2025-12-28 17:39:45 +0100

    Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,10 @@
 fx_version 'cerulean'
-games { 'gta5' }
-author 'luman'
+game 'gta5'
+
+name 'My Resource'
+author 'Luman Studio'
 version '1.0.0'
-this_is_a_map 'yes'
+description 'PvP arena for FiveM'
 
 
 files {
